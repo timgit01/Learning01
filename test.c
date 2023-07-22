a3e8e9ebe65ba7d64819b7398d11245b19202ed4
Anish Pechetti <anishpechetti01@gmail.com>
2023-07-22 13:03:36 +0530

    Added age functionality

--- test.c
+++ test.c
@@ -2,12 +2,39 @@
 int main()
 {
     printf("\nHi there!");
-    int age, i;
+    int i, age;
     char first_name[20], last_name[20], answer[5];
     printf("\nEnter your first name: ");
     scanf("%s", &first_name);
     printf("Enter your last name: ");
     scanf("%s", &last_name);
     printf("\nHello, %s %s.", first_name, last_name);
+    printf("\nEnter your age: ");
+    scanf("%d", &age);
+    if (age > 18)
+    {
+        printf("\nYou are eligible to vote.");
+        if (age > 30)
+        {
+            printf("\nYou are also eligible to run elections.");
+            printf("\nWould you like to run elections: ");
+            scanf("%s", &answer);
+            switch (answer[0])
+            {
+            case 'y':
+                printf("\nPlease contact election committee.");
+                break;
+            case 'n':
+                printf("\nYou can continue with voting.");
+            default:
+                printf("Invalid input.");
+                break;
+            }
+        }
+    }
+    else
+    {
+        printf("\nYou are below 18, You cannot vote.");
+    }
     return 0;
 }
