edec705d7fb35d2b768ad527310ba73d71f0c6a4
Anish Pechetti <anishpechetti01@gmail.com>
2023-07-22 10:00:04 +0530

    Added name functionality

--- test.c
+++ test.c
@@ -2,5 +2,12 @@
 int main()
 {
     printf("\nHi there!");
+    int age, i;
+    char first_name[20], last_name[20], answer[5];
+    printf("\nEnter your first name: ");
+    scanf("%s", &first_name);
+    printf("Enter your last name: ");
+    scanf("%s", &last_name);
+    printf("\nHello, %s %s.", first_name, last_name);
     return 0;
 }
